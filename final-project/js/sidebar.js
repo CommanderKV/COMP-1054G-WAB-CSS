ef9bac43e42eff9e4675f17d64074218591abc74
CommanderKV <kylervisser@gmail.com>
2024-03-12 19:57:42 -0400

    Fixed sidebar for mobile. NEXT: sidebar button seeability fixing

--- final-project/js/sidebar.js
+++ final-project/js/sidebar.js
@@ -1,9 +1,15 @@
-/* Set the width of the side navigation to 250px */
-function openNavigation() {
-    document.querySelector(".navigation").style.width = "250px";
-}
-  
-/* Set the width of the side navigation to 0 */
-function closeNavigation() {
-    document.querySelector(".navigation").style.width = "0";
+
+let navOpen = false;
+function toggleNavigation() {
+    if (navOpen) {
+        // Set the width of the navigation to 0 to hide it
+        document.querySelector(".navigation").style.width = "0";
+        
+    } else {
+        // Set the width of the navigation to 60% to show it
+        document.querySelector(".navigation").style.width = "60%";
+    }
+
+    // Toggle the value of navOpen
+    navOpen = !navOpen;
 }

4f4cc16d8c5e27d1e97e1231519545f4e5507152
CommanderKV <kylervisser@gmail.com>
2024-03-12 17:30:04 -0400

    Added new sidebar when screen gets smaller. Doesn't work on opera. Must fix

---
+++ final-project/js/sidebar.js
@@ -0,0 +1,9 @@
+/* Set the width of the side navigation to 250px */
+function openNavigation() {
+    document.querySelector(".navigation").style.width = "250px";
+}
+  
+/* Set the width of the side navigation to 0 */
+function closeNavigation() {
+    document.querySelector(".navigation").style.width = "0";
+}

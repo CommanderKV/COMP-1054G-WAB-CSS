896ac1abb7086e8ad6cafb78a449e3d645f53ee9
CommanderKV <kylervisser@gmail.com>
2024-03-16 02:25:11 -0400

    Completed home page

--- final-project/js/sidebar.js
+++ final-project/js/sidebar.js
@@ -1,4 +1,10 @@
+// On window resize adjust sidebar
+window.addEventListener("resize", function() {
+    document.querySelector(".navigation").removeAttribute("style");
+});
 
+
+// Navigation menu
 let navOpen = false;
 function toggleNavigation() {
     if (navOpen) {
@@ -7,9 +13,19 @@ function toggleNavigation() {
         
     } else {
         // Set the width of the navigation to 60% to show it
-        document.querySelector(".navigation").style.width = "60%";
+        document.querySelector(".navigation").style.width = "100%";
     }
 
     // Toggle the value of navOpen
     navOpen = !navOpen;
 }
+
+// Adjust service dropdown
+document.querySelector(".services-arrow").addEventListener("click", function() {
+    var dropdown = document.querySelector(".services-dd");
+    if (dropdown.style.display === "flex") {
+        dropdown.style.display = "none";
+    } else {
+        dropdown.style.display = "flex";
+    }
+});

c5f6ea68729889b109fe0b946ae9f3c2ff094808
CommanderKV <kylervisser@gmail.com>
2024-03-21 22:27:03 -0400

    Minor fixes

--- final-project/index.php
+++ final-project/index.php
@@ -17,25 +17,26 @@
                 <h1>Home</h1>
             </section>
             <section class="home">
-                <section class="about">
+                <div class="about">
                     <h2>About me</h2>
                     <p>I am a Computer science student studying in the Lakehead-Georgian program. This means that I will study 2 years at Georgian college and 2 years at Lakehead Georgian </p>
-                </section>
-                <section class="expertise">
+                </div>
+                <div class="expertise">
                     <h2>Expertise</h2>
                     <p>Being in computer science I have a very deep and through understanding of many programming languages.</p>
-                </section>
-                <section class="services">
+                </div>
+                <div class="services">
                     <h2>Services</h2>
-                    <p>There are a few services that I offer. These include butare not limited to<br>
+                    <p>There are a few services that I offer. These include but are not limited to<br>
                         &checkmark;Website dev<br>
                         &checkmark;Python dev<br>
                         &checkmark;Php dev<br>
                         &checkmark;Java dev<br>
                         &checkmark; ...<br>
                     </p>
-                </section>
+                </div>
             </section>
+            
 
         </main>
         <footer>

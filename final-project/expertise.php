c173a79f034ea3a32307f2550dd5c07a5a5eced9
CommanderKV <kylervisser@gmail.com>
2024-03-30 19:06:24 -0400

    Added some fonts and content to expertise

--- final-project/expertise.php
+++ final-project/expertise.php
@@ -13,6 +13,24 @@
             <section class="masthead f-display">
                 <h1>Expertise</h1>
             </section>
+            <section class="expertise">
+                <div>
+                    <h2>Programming</h2>
+                    <p>I have a very diverse understanding of many different programming languages. I have a very deep understanding of Python to go with that I've alost worked with Java, C++, and PHP. I also have a good understanding of HTML, CSS, and JavaScript. I have also worked with SQL so I know how to do the very basics of database management.</p>
+                </div>
+                <div>
+                    <h2>Web Development</h2>
+                    <p>I have a well versed understanding of web development. I have worked with HTML, CSS, JavaScript, and PHP. I have also worked with SQL so I know how to do the very basics of database management. I've developed a few websites including this one by myself with minimal help. This was accomplished through my CSS class, Web fundamentals class, and my Javascript class.</p>
+                </div>
+                <div>
+                    <h2>Software Development</h2>
+                    <p>I have a decent understanding of how to develop software and the processes included. I've worked on multiple projects that involved raspberry pi's and operating systems. I've developed a fan controller for a raspberry pi which included making software that would be able to control the output to the fans as well as manage the user interface as well. In addition to that I've also recreated multiple games using pygame with python.</p>
+                </div>
+                <div>
+                    <h2>AI development</h2>
+                    <p>I am still developing my skills when it comes to AI, however, I have worked on some projects already involving the games that I recreated. I've made a flappy bird AI, a snake AI, and a few more. I would not say that I can make very complicated AI's as I don't work to much in that realm, however, if need be I can make an attempt.</p>
+                </div>
+            </section>
         </main>
         <footer>
             <?php require "includes/footer.html"; ?>
